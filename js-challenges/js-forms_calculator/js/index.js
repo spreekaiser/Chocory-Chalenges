19e969c42ac4ed6eec30c7c19b465eaec3f3edb1
Thoralf Barth <thoralf.barth@gmail.com>
2023-04-27 16:06:53 +0200

    practizing to read out objects

--- js-challenges/js-forms_calculator/js/index.js
+++ js-challenges/js-forms_calculator/js/index.js
@@ -1,6 +1,14 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+const numberA = document.querySelector("#number-a");
+const numberB = document.querySelector("#number-b");
+
+const addition = document.querySelector("#addition");
+const subtraction = document.querySelector("#subtraction");
+const multiplication = document.querySelector("#multiplication");
+const division = document.querySelector("#division");
+
 const resultOutput = document.querySelector('[data-js="result"]');
 
 function add(a, b) {
@@ -21,11 +29,25 @@ function divide(a, b) {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
   let result;
 
   // --v-- write your code here --v--
-
+  if (addition.checked) {
+    result = add(parseInt(numberA.value), parseInt(numberB.value));
+    console.log(result);
+  }
+  if (subtraction.checked) {
+    result = subtract(parseInt(numberA.value), parseInt(numberB.value));
+    console.log(result);
+  }
+  if (multiplication.checked) {
+    result = multiply(parseInt(numberA.value), parseInt(numberB.value));
+    console.log(result);
+  }
+  if (division.checked) {
+    result = divide(parseInt(numberA.value), parseInt(numberB.value));
+    console.log(result);
+  }
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
