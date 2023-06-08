291f695b61acf20e4d721da48435c008d0f46269
spreekaiser <thoralf.barth@gmail.com>
2023-06-08 10:04:09 +0200

    last bug fixes backend-challenges

--- backend-challenges/backend-read/backend-read_products/pages/api/products/[id].js
+++ backend-challenges/backend-read/backend-read_products/pages/api/products/[id].js
@@ -1,5 +1,4 @@
 import Product from "../../../db/models/Product";
-// import dbConnect from "../../../db/connect";
 
 export default async function handler(request, response) {
   console.log("request.query: ", request.query);
@@ -10,8 +9,4 @@ export default async function handler(request, response) {
     return response.status(404).json({ status: "Not Found" });
   }
   return response.status(200).json(product);
-
-  // await dbConnect();
-
-  // }
 }
