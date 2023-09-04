981767f47885c90701f1f0b1251c634768af95e1
spreekaiser <thoralf.barth@gmail.com>
2023-09-04 10:53:00 +0200

    cat and mouse

--- codewars/TS_kata.ts
+++ codewars/TS_kata.ts
@@ -16,3 +16,19 @@ function findShort(s: string): number {
 }
 
 findShort("bitcoin take over the world maybe who knows perhaps");
+
+// You will be given a string (x) featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'.
+// You need to find out if the cat can catch the mouse from it's current position. The cat can jump over three characters. So:
+// C.....m returns 'Escaped!' <-- more than three characters between
+// C...m returns 'Caught!' < --as there are three characters between the two, the cat can jump.
+export function catMouse(x: string): string {
+  if (x.indexOf("m") - x.indexOf("C") > 4) {
+    return "Escaped!";
+  } else {
+    return "Caught!";
+  }
+}
+
+catMouse("C...m");
+
+//
